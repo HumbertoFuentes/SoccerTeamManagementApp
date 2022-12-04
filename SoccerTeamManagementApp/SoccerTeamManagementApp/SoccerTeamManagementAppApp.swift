9d6616f6205855a38911f58018d6eff26ce272d8
HumbertoFuentes <hfuente2@asu.edu>
2022-12-03 17:51:41 -0700

    Project Init Commit

---
+++ SoccerTeamManagementApp/SoccerTeamManagementApp/SoccerTeamManagementAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoccerTeamManagementAppApp.swift
+//  SoccerTeamManagementApp
+//
+//  Created by Humberto Fuentes on 12/3/22.
+//
+
+import SwiftUI
+
+@main
+struct SoccerTeamManagementAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
